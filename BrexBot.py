6991dcfc883c454aac969a9271be5fea211cc8dc
Børge Jakobsen <borge.jakobsen@gmail.com>
2019-09-06 20:45:51 +0200

    Added header

--- BrexBot.py
+++ BrexBot.py
@@ -1,1 +1,8 @@
-# This is a test
+__author__ = "Børge Jakobsen, Thomas Donegan"
+__copyright__ = "Copyright 2019, Brexit boy and SaLmon king"
+__credits__ = ["Børge Jakobsen, Thomas Donegan"]
+__license__ = "MIT"
+__version__ = "1.0"
+__maintainer__ = "Børge Jakobsen, Thomas Donegan"
+__status__ = "Development"
+
