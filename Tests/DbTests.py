78a28703bbfc03699f1edd167aa77500d12b59b0
Børge Jakobsen <borge.jakobsen@gmail.com>
2019-09-07 12:52:13 +0200

    Created tests folder. Moved db tests from BrexBotTests.py to DbTests.py

--- Tests/DbTests.py
+++ Tests/DbTests.py
@@ -1,10 +1,8 @@
 import unittest
 from Db import Db
 
-class Test(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)
 
+class Test(unittest.TestCase):
     def test_user_exists_in_db(self):
         twitchusername = "bob"
         res = Db().viewer_exists(twitchusername)
@@ -21,7 +19,6 @@ class Test(unittest.TestCase):
         points = 100
         res = Db().remove_points(twitchusername, points)
         self.assertEqual(res, True)
-
 
 if __name__ == '__main__':
     unittest.main()
