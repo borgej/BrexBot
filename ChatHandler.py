c23ca4cb04396e136a53f64b64e9eb9b50156062
Børge Jakobsen <borge.jakobsen@gmail.com>
2019-09-10 22:33:39 +0200

    # Added initial ChatHandler.py # Usable class for chat interaction/monitoring

---
+++ ChatHandler.py
@@ -0,0 +1,59 @@
+import twitch
+import logging
+
+
+class ChatHandler(twitch):
+    def __init__(self, channelname, nickname, oauth):
+        self.nickname = nickname
+        self.channel = "#" + channelname
+        self.oauth = oauth
+        self.chat_connection = None
+
+        self.chat_log = []
+        self.triggers = None
+        self.polls = None
+        self.banned_words = None
+
+        self.logger = logging.getLogger()
+
+    # Log chatmessage
+    def log_message(self, chat_message):
+        self.chat_log.append(chat_message)
+
+    # Update triggers if changed outside ChatHandler
+    def set_triggers(self, triggers):
+        self.triggers = triggers
+        self.logger.info("Updated triggers")
+
+    # Set banned words
+    def set_banned_words(self, banned_words):
+        self.banned_words = banned_words
+        self.logger.info("Updated banned words")
+
+    # Set polls
+    def set_polls(self, polls):
+        self.polls = polls
+        self.logger.info("Updated polls")
+
+    # Get the chat log
+    def get_chat_log(self):
+        return self.chat_log
+
+    # Connect to chat
+    def connect_to_chat(self, message_checker):
+        try:
+            self.chat_connection = self.Chat(channel=self.channel, nickname=self.nickname, oauth=self.oauth)
+            # call message_checker on every received message
+            self.chat_connection.subscribe(lambda message: message_checker(message))
+            # Log every message
+            self.chat_connection.subscribe(lambda message: self.log_message(message))
+
+            self.send_message("I just connected...")
+            self.logger.info("Connected to channel: " + self.channel)
+            return self.chat_connection
+        except Exception as e:
+            self.logger.exception("Error on connecting to channel: " + self.channel)
+
+    # Send message to chat
+    def send_message(self, message):
+        self.chat_connection.send(self.chat)
---
+++ Tests/TwitchTest.py
@@ -0,0 +1,33 @@
+from twitch import TwitchClient
+
+class TwitchTest:
+    def __init__(self):
+        #Create a client using the "TwitchClient" connection
+        # self.client = TwitchClient(client_id='i6a33w342uxukr28axfjppf0zvsrbz', oauth_token='oauth:qdg13wxdcuuovuyl08sf1ar84h7sjx')
+        self.client = TwitchClient('gokkk5ean0yksozv0ctvljwqpceuin', 'oauth:gox8ache7pazv7lufoaio4es6v3d4u')
+
+    def get_id(self, username):
+        user_info = self.client.users.translate_usernames_to_ids(username)
+        return user_info[0].get("id")
+
+    def get_channel_info(self, username):
+        channel_id = self.get_id(username)
+        channel_info = self.client.channels.get_by_id(channel_id)
+        print(channel_info)
+
+    # This wont work until the application is authenticated on the channel
+    def check_if_subscribed(self, viewer, channel):
+        viewer_id = self.get_id(viewer)
+        channel_id =  self.get_id(channel)
+        is_subscribed = self.client.users.check_subscribed_to_channel(viewer_id, channel_id)
+        print(is_subscribed)
+        #return self.client.users.check_subscribed_to_channel(viewer_id, channel_id)
+
+
+con = TwitchTests()
+#con.user_follows()
+channelId = con.get_id("beejeey")
+channelInfo = con.client.channels.get_followers(channelId, 100, 0, None)
+print(channelInfo)
+
+con.check_if_subscribed("tomisme", "beejeey")
