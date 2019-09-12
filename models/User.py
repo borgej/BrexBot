07842265e0183ffddedded67ffb82d02b0f9ddf7
Børge Jakobsen <borge.jakobsen@gmail.com>
2019-09-12 17:50:41 +0200

    Added User.py model

---
+++ models/User.py
@@ -0,0 +1,19 @@
+__author__ = "Børge Jakobsen, Thomas Donegan"
+__copyright__ = "Copyright 2019, Brexit boy and SaLmon king"
+__credits__ = ["Børge Jakobsen, Thomas Donegan"]
+__license__ = "Apache License"
+__version__ = "2.0"
+__maintainer__ = "Børge Jakobsen, Thomas Donegan"
+__status__ = "Development"
+
+class User():
+    def __init__(self, username, channel, bot_name, bot_oauth, channel_token, created, last_active):
+        self.username = username
+        self.channel = channel
+        self.bot_name = bot_name
+        self.bot_oauth = bot_oauth
+        self.channel_token = channel_token
+        self.created = created
+        self.last_active = last_active
+
+
