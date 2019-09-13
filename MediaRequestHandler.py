353a1abcd8bd1922800eb9d93a02851accb79907
Børge Jakobsen <borge.jakobsen@gmail.com>
2019-09-13 21:40:07 +0200

    *Fixed MediaRequestHandler *Tests

--- MediaRequestHandler.py
+++ MediaRequestHandler.py
@@ -7,12 +7,11 @@ __maintainer__ = "Børge Jakobsen, Thomas Donegan"
 __status__ = "Development"
 
 import logging
+import json
+import pprint
+import urllib
 import urllib.request
 import urllib.parse
-import re
-import requests
-import pprint
-from lxml import html
 from models.MediaRequest import MediaRequest
 
 logging.basicConfig(level=logging.DEBUG)
@@ -21,47 +20,58 @@ logging.basicConfig(level=logging.DEBUG)
 # TODO: - Add tests
 #       - Get title from chosen result
 #       - Get number of views from chosen result
+#       - Get length of video
 class MediaRequestHandler():
     def __init__(self):
-        pass
+        self.api_key = "AIzaSyBKpwIdN7OFlEXYvqGkkLBiBT4x0vIEkrU"
 
     def search_request(self, request: MediaRequest):
         try:
             if(request.title == None):
                 logging.debug("No title in request, not searching")
                 return None
-
             logging.debug("Searching for: '" + request.title +"'")
 
-            query_string = urllib.parse.urlencode({"search_query": request.title})
-            html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
-            html_content = html_content.read().decode()
-            search_results_video_id = re.findall(r'href=\"\/watch\?v=(.{11})', html_content)
+            search_url_snippet = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&order=relevance&q=" + urllib.parse.quote(request.title) + "&key=" + self.api_key
+            response_snippet = urllib.request.urlopen(search_url_snippet).read()
+            dataSnippet = json.loads(response_snippet)
 
-            if(len(search_results_video_id) > 0):
-                hits = len(search_results_video_id)
-                logging.debug("Found " + str(hits) + " hits for '" + request.title +"'")
-                request.video_id = search_results_video_id[0]
-                request.url = "http://www.youtube.com/watch?v=" + request.video_id
-                logging.debug("Search for '" + request.title + "' gave video_id: " + request.video_id + " and url: " + request.url)
-                return request
-        except ConnectionError:
+            logging.debug("Found " + str(len(dataSnippet['items'])) + " matches for '" + request.title +"'")
+
+            all_data_snippet = dataSnippet['items']
+            request.video_id = all_data_snippet[0]['id']['videoId']
+            request.title = all_data_snippet[0]['snippet']['title']
+            request.url = "https://www.youtube.com/watch?v=" +request.video_id
+
+            logging.debug("Selected " + request.title + " with url " + request.url)
+
+            return request
+        except:
             logging.exception("Exception in search call to YouTube ")
             return None
 
     def get_video_details(self, request: MediaRequest):
         try:
-            pp = pprint.PrettyPrinter(indent=4)
-            html_content = urllib.request.urlopen(request.url)
-            tree = html.fromstring(html_content.read())
+            search_url_snippet = "https://www.googleapis.com/youtube/v3/videos?id=" + request.video_id + "&key=" + self.api_key + "&part=snippet"
+            search_url_content_details = "https://www.googleapis.com/youtube/v3/videos?id=" + request.video_id + "&key=" + self.api_key + "&part=contentDetails"
 
-            request.title = tree.xpath('*//ytd-video-primary-info-renderer/div/h1/yt-formatted-string')
-            request.length = tree.xpath('*//ytd-page-manager/ytd-watch-flexy/div[3]/div[1]/div/div[1]/div/div/div/ytd-player/div/div/div[29]/div[2]/div[1]/div/span[3]')
-            
-            pp.pprint(request.title)
-            pp.pprint(request.length)
+            response_snippet = urllib.request.urlopen(search_url_snippet).read()
+            response_content_details = urllib.request.urlopen(search_url_content_details).read()
 
-            logging.debug("Video details gave '" + request.title + "' with a length of: " + request.length)
+            dataSnippet = json.loads(response_snippet)
+            dataContentDetails = json.loads(response_content_details)
+
+            all_data_snippet = dataSnippet['items']
+            all_data_content_details = dataContentDetails['items']
+            contentDetails = all_data_snippet[0]['snippet']
+            length = all_data_content_details[0]['contentDetails']
+
+            request.title = contentDetails['title']
+            request.thumbnail_url = contentDetails['thumbnails']['default']['url']
+            request.length = length['duration']
+
+            logging.debug("Video id: " + request.video_id + " gave this data: title: " + request.title + " length: " + request.length )
+
             return request
         except:
             logging.exception("Exception in get_video call to YouTube")
--- Tests/MediaRequestTest.py
+++ Tests/MediaRequestTest.py
@@ -12,15 +12,18 @@ class MediaRequestTest(unittest.TestCase):
 
         song_request = handler.search_request(song_request)
 
+        self.assertIsNotNone(song_request)
+        self.assertIsNotNone(song_request.video_id)
         self.assertIsNotNone(song_request.url)
 
     def test_get_video_details(self):
         handler = MediaRequestHandler()
-        song_request = MediaRequest("Aqua Barbie Girl")
+        song_request = MediaRequest("I Built A Giant House Using Only Legos")
+        song_request.video_id = "9vB-48kHbBU"
 
-        song_request = handler.search_request(song_request)
         song_request = handler.get_video_details(song_request)
 
+        self.assertIsNotNone(song_request)
         self.assertIsNotNone(song_request.title)
         self.assertIsNotNone(song_request.length)
 
